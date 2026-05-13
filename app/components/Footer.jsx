55e74775769e9ad17f907c53e0d73f5a4bfd2c42
Matheus <matheusfloliveira@gmail.com>
2026-05-12 21:21:06 -0300

    Footer component created

---
+++ app/components/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+    return (
+        <footer className='py-[50px] text-center border-t border-[rgba(33,49,49,0.1)] text-[0.75rem] text-[#213131] opacity-60 tracking-[1px]'>
+            <p>© 2026 MOM Ateliê — Feito com amor por você.</p>
+        </footer>
+    )
+}
